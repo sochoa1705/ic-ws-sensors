d139141aca0f1bcd725787bd047b001c030e44c1
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2023-07-25 14:07:52 -0500

    fix db connection

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import paho.mqtt.client as mqtt
 import json
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/dashboard_incendios_forestales'
+app.config['MONGO_URI'] = 'mongodb://mongo:bsYUWlge9whaYGDPcBi3@containers-us-west-68.railway.app:6326/dashboard_incendios_forestales'
 mongo = PyMongo(app)
 live_data = []
 
